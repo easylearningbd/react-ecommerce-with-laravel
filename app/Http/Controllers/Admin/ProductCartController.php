4522f0c16d3b0a89d6c19a4a63007c1f3d483f09
easylearningbd <elebdsoft@gmail.com>
2021-08-16 02:06:18 +0600

    134 Add To Cart Part 2

--- app/Http/Controllers/Admin/ProductCartController.php
+++ app/Http/Controllers/Admin/ProductCartController.php
@@ -5,11 +5,46 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\ProductCart;
+use App\Models\ProductList;
 
 class ProductCartController extends Controller
 {
     public function addToCart(Request $request){
+        $email = $request->input('email');
+        $size = $request->input('size');
+        $color = $request->input('color');
+        $quantity = $request->input('quantity');
+        $product_code = $request->input('product_code');
 
+        $productDetails = ProductList::where('product_code',$product_code)->get();
+
+        $price = $productDetails[0]['price'];
+        $special_price = $productDetails[0]['special_price'];
+
+        if($special_price=="na"){
+            $total_price = $price*$quantity;
+            $unit_price = $price;
+        }
+        else{
+            $total_price = $special_price*$quantity;
+            $unit_price = $special_price;
+        }
+
+        $result = ProductCart::insert([
+
+            'email' => $email,
+            'image' => $productDetails[0]['image'],
+            'product_name' => $productDetails[0]['title'],
+            'product_code' => $productDetails[0]['product_code'],
+            'size' => "Size: ".$size,
+            'color' => "Color: ".$color,
+            'quantity' => $quantity,
+            'unit_price' => $unit_price,
+            'total_price' => $total_price, 
+
+        ]);
+
+        return $result;
     } // End Method 
 
 
