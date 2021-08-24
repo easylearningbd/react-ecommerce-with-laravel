e588876685ff0728027145452edbd8ce9b9816fc
easylearningbd <elebdsoft@gmail.com>
2021-08-24 06:17:26 +0600

    a2 Setup Admin Theme

--- routes/web.php
+++ routes/web.php
@@ -18,5 +18,5 @@ Route::get('/', function () {
 });
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
-    return view('dashboard');
+    return view('admin.index');
 })->name('dashboard');
